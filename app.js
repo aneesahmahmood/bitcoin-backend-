d491e16308eb7c5073b152e796bf58a36db78d7b
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-07-10 15:28:16 +0100

    added json file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+const endpoints = require("./endpoint.json");
 
 const app = express();
 app.use(express.json());
@@ -8,3 +10,8 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get("/bitcoinprice", (req, res) => {
+  res.status(200).json(endpoints);
+});
+
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const axios = require("axios");
 require("dotenv").config();
 
-axios
-  .get(`https://pro-api.coinmarketcap.com/v2/tools/price-conversion??api_key=${process.env.COINMARKETCAP_API_KEY}`)
-  .then((response) => {
-    console.log(response.data);
-  });
-// need to have a route. need a route called /bitcoin price. get requet so should return empty object 
+
+// axios
+//   .get(
+//     `https://pro-api.coinmarketcap.com/v2/tools/price-conversion??api_key=${process.env.COINMARKETCAP_API_KEY}`
+//   )
+//   .then((response) => {
+//     console.log(response.data);
+//   });
+// need to have a route. need a route called /bitcoin price. get requet so should return empty object
 // axios
 //   .get(
 //     `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`
@@ -14,3 +17,11 @@ axios
 //   .then((response) => {
 //     console.log(response.data);
 //   });
+
+// index.get('/apis', (req, res) => {
+//   res.status(200).json(endpoints);
+// });
+
+// index.get("/bitcoinprice", (req, res) => {
+//   res.status(200).json({});
+// });
